44c09dc87fe3dd32f0417d7f19566275e854d737
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2019-03-17 00:38:07 +0100

    added resizing, drawing and validating input data

--- Lab03-Tic-Tac-Toe/src/TicTacToe.java
+++ Lab03-Tic-Tac-Toe/src/TicTacToe.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
 
 public class TicTacToe extends JPanel
 {
@@ -17,15 +18,64 @@ public class TicTacToe extends JPanel
     {
         JFrame window = new JFrame("Tic-Tac-Toe");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setBounds(300,200,600,800);
+        window.setBounds(300,200,600,700);
         window.setResizable(false);
 
         JPanel panelik = new JPanel();
         panelik.setLayout(new BoxLayout(panelik,BoxLayout.PAGE_AXIS));
         JPanel settingsPanel = new JPanel();
 
-        panelik.setMaximumSize(new Dimension(600,800));
-        settingsPanel.setMaximumSize(new Dimension(600,200));
+        JLabel size_xLabel = new JLabel("Size X");
+        JLabel size_yLabel = new JLabel("Size Y");
+        JLabel winLabel = new JLabel("Win count");
+
+        JFormattedTextField xSizeText = new JFormattedTextField(Integer.toString(sizeX));
+        JFormattedTextField ySizeText = new JFormattedTextField(Integer.toString(sizeY));
+        JFormattedTextField winSizeText = new JFormattedTextField(Integer.toString(winSize));
+
+        xSizeText.setPreferredSize(new Dimension(75,24));
+        ySizeText.setPreferredSize(new Dimension(75,24));
+        winSizeText.setPreferredSize(new Dimension(75,24));
+
+        JButton submitChanges = new JButton("Reset/Apply setting");
+
+        submitChanges.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                actionLogic();
+            }
+
+            private void actionLogic(){
+                try {
+                    sizeX = Integer.parseInt(xSizeText.getText());
+                    sizeY = Integer.parseInt(ySizeText.getText());
+                    winSize = Integer.parseInt(winSizeText.getText());
+                }catch ( NumberFormatException  e){
+                    JOptionPane.showMessageDialog(null,"Wrong input data");
+                    xSizeText.setText(Integer.toString(sizeX));
+                    ySizeText.setText(Integer.toString(sizeY));
+                    winSizeText.setText(Integer.toString(winSize));
+                    return;
+                }
+
+                xSizeText.setText(Integer.toString(sizeX));
+                ySizeText.setText(Integer.toString(sizeY));
+                winSizeText.setText(Integer.toString(winSize));
+
+                setupTicTacToe();
+            }
+        });
+
+        settingsPanel.add(size_xLabel);
+        settingsPanel.add(xSizeText);
+        settingsPanel.add(size_yLabel);
+        settingsPanel.add(ySizeText);
+        settingsPanel.add(winLabel);
+        settingsPanel.add(winSizeText);
+        settingsPanel.add(submitChanges);
+
+        panelik.setMaximumSize(new Dimension(600,700));
+        settingsPanel.setMaximumSize(new Dimension(600,100));
         this.setMaximumSize(new Dimension(600,600));
 
         panelik.add(settingsPanel);
@@ -54,10 +104,11 @@ public class TicTacToe extends JPanel
                 buttons[i][j].setText("");
                 buttons[i][j].setFont(new Font("Arial", Font.PLAIN, 20));
                 buttons[i][j].addActionListener(new buttonListener());
-                add(buttons[i][j]); //adds this button to JPanel (note: no need for JPanel.add(...)
-                //because this whole class is a JPanel already
+                add(buttons[i][j]);
             }
         }
+        revalidate();
+        repaint();
     }
     private void resetButtons()
     {
@@ -86,6 +137,20 @@ public class TicTacToe extends JPanel
                 alternate++;
             }
 
+            boolean reset =true;
+
+            for (int i = 0; i < sizeY; i++) {
+                for (int j = 0; j < sizeX; j++) {
+                    if (buttons[i][j].getText().equals("")){
+                        reset = false;
+                    }
+                }
+            }
+
+            if (reset){
+                JOptionPane.showMessageDialog(null, "Draw... Restarting!");
+                resetButtons();
+            }
 
             if(checkForWin())
             {
