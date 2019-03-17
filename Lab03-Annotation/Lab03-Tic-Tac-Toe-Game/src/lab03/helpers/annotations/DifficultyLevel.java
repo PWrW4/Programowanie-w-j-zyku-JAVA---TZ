a48709d439473b3031cd29bf6486542365d65598
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2019-03-17 21:25:44 +0100

    anotation reading

--- Lab03-Annotation/Lab03-Tic-Tac-Toe-Game/src/lab03/helpers/annotations/DifficultyLevel.java
+++ Lab03-Annotation/Lab03-Tic-Tac-Toe-Game/src/lab03/helpers/annotations/DifficultyLevel.java
@@ -2,6 +2,13 @@ package lab03.helpers.annotations;
 
 import lab03.helpers.enumerators.Difficulty;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface DifficultyLevel {
     Difficulty level();
 }
--- Lab03-Annotation/Lab03-Tic-Tac-Toe-Game/src/lab03/ticTacToeModules/AIControl.java
+++ Lab03-Annotation/Lab03-Tic-Tac-Toe-Game/src/lab03/ticTacToeModules/AIControl.java
@@ -1,18 +1,26 @@
 package lab03.ticTacToeModules;
 
+import lab03.helpers.annotations.DifficultyLevel;
 import lab03.helpers.annotations.TicTacToeAI;
 
 import java.io.File;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
 
 public class AIControl {
 
     URLClassLoader urlClassLoader;
     Class aiClass;
+    Object aiObject;
+    Constructor<?> aiClassConstructor;
 
-
+    Method easy, medium, hard;
 
 
     public boolean setClass(String classPathURL, String fileName) {
@@ -20,16 +28,15 @@ public class AIControl {
         System.out.println(classPathURL);
         System.out.println(fileName);
 
-        classPathURL = classPathURL.replaceAll(fileName,"");
+        classPathURL = classPathURL.replaceAll(fileName, "");
 
         System.out.println(classPathURL);
         System.out.println(fileName);
 
         String className = fileName.replaceFirst("[.][^.]+$", "");
 
-
         try {
-            urlClassLoader = new URLClassLoader(new URL[] {new URL(classPathURL)});
+            urlClassLoader = new URLClassLoader(new URL[]{new URL(classPathURL)});
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -39,6 +46,39 @@ public class AIControl {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+        try {
+            aiClassConstructor = aiClass.getConstructor();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        try {
+            aiObject = aiClassConstructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        Method[] methods = aiClass.getDeclaredMethods();
+
+        ArrayList<Annotation[]> annotations = new ArrayList<>();
+
+        for (Method method :
+                methods) {
+            if (method.isAnnotationPresent(DifficultyLevel.class)) {
+                switch (method.getAnnotation(DifficultyLevel.class).level()) {
+                    case Hard:
+                        hard = method;
+                        break;
+                    case Medium:
+                        medium = method;
+                        break;
+                    case Easy:
+                        easy = method;
+                        break;
+                }
+            }
+        }
+
 
         return aiClass.isAnnotationPresent(TicTacToeAI.class);
 
